86724801d316c911e39c596f1a25193bb3a746ea
parzonka <parzonka@gmail.com>
2020-09-24 21:36:27 +0000

    configure java 14

--- java-junit4/src/main/java/Main.java
+++ java-junit4/src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" + new Person("foo", "bar"));
+    }
+
+    public static record Person(String name, String partner) {
     }
 
 }
